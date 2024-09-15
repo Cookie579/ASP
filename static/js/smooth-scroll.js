e877cad7f9f186b8011bccee1e062412c9227dcb
Agasthya <agasthya.mc@gmail.com>
2024-09-15 19:08:25 -0400

    Test Lenis

--- static/js/smooth-scroll.js
+++ static/js/smooth-scroll.js
@@ -1,14 +1,27 @@
-const lenis = new Lenis({
-  duration: 1.2,
-  easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
-  direction: "vertical",
-  gestureDirection: "vertical",
-  smooth: true,
-  smoothTouch: false,
-  touchMultiplier: 2,
-});
+// const lenis = new Lenis({
+//   duration: 1.2,
+//   easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
+//   direction: "vertical",
+//   gestureDirection: "vertical",
+//   smooth: true,
+//   smoothTouch: false,
+//   touchMultiplier: 2,
+// });
+// function raf(time) {
+//   lenis.raf(time);
+//   requestAnimationFrame(raf);
+// }
+// requestAnimationFrame(raf);
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
 function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
+  lenis.raf(time)
+  requestAnimationFrame(raf)
 }
-requestAnimationFrame(raf);
+
+requestAnimationFrame(raf)
