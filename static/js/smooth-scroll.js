d52d6c4c0585c87e6905187d19a114836929b47e
Agasthya <agasthya.mc@gmail.com>
2024-09-15 16:41:48 -0400

    Updated to Lenis Scroll

--- static/js/smooth-scroll.js
+++ static/js/smooth-scroll.js
@@ -1,4 +1,14 @@
-const scroll = new LocomotiveScroll({
-  el: document.querySelector('body.is-preload'),
-  smooth: true
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
+  direction: "vertical",
+  gestureDirection: "vertical",
+  smooth: true,
+  smoothTouch: false,
+  touchMultiplier: 2,
 });
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
